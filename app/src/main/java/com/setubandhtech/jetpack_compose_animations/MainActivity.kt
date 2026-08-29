e8abac666a876da798cdc31a875166c1780fd603
krishvekriya12 <krishvekriya44@gmail.com>
2026-08-29 08:05:46 +0530

    update code

--- app/src/main/java/com/setubandhtech/jetpack_compose_animations/MainActivity.kt
+++ app/src/main/java/com/setubandhtech/jetpack_compose_animations/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import com.setubandhtech.jetpack_compose_animations.flappybird.FlappyBird
 import com.setubandhtech.jetpack_compose_animations.ui.theme.JetpackcomposeanimationsTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    FlappyBird()
+
                 }
             }
         }
